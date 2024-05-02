ae5bce8d8f6e74f0e14ec6970f5a6ec127173c59
Selma <146540483+selmabou@users.noreply.github.com>
2024-05-02 21:05:56 +0100

    class Tasks

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-Taches = []
+class Gestion_Taches:
 
-def ajouter_tache() :
-    tache = input(str("saisir une tache .. "))
-    Taches.append(tache)
-    print(f"La tache : '{tache} , ajoute a la list .'")
-
-
-
-def supprimer_tache (Taches , index):
-    if index >= 0 and index < len(Taches):
-        del Taches[index]
-        print ("tache est supprimer")
-
-    else:
-        print("list des taches vide !!") 
+    def __init__(self):
+        self.Table_taches = []
 
 
 
