cc4f81e50946bc17a27c48beb6e326e439297748
Selma <146540483+selmabou@users.noreply.github.com>
2024-05-02 21:07:24 +0100

    add task

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ class Gestion_Taches:
     def __init__(self):
         self.Table_taches = []
 
+    def ajouter_tache(self, tache):
+        self.Table_taches.append(tache)
+        print(" ->  Tache ajoutee . ")
+
 
 
           
